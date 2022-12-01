4d4899cd311315d9048a6cd03dbfa0911991e1c2
awgore99 <awgore99@gmail.com>
2022-12-01 10:56:55 -0600

    Added html routes to server.js

--- server.js
+++ server.js
@@ -7,3 +7,14 @@ var PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(express.static("public"));
+
+// Routing for HTML
+//      Route for index.html
+app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
+//      Route for notes.html
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
